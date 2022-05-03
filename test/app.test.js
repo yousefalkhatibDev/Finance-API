93a69b5149ac9ffb0435dae32795465a249ff89b
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-05-03 15:08:46 +0300

    made test file

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const server = require("../server")
+const supertest = require("supertest")
+const request = supertest(server)
+
+describe("/test endpoint", () => {
+    it("should return a response", async () => {
+        const response = await request.get("/test")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("Hello world");
+    })
+})
