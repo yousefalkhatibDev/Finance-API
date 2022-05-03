d72c5fbb0422c1079ac3547f94914ccb9ed50bb4
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-05-03 15:56:07 +0300

    new test

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,14 @@ const routes = require("../routers/routes")
 const supertest = require("supertest")
 const request = supertest(routes)
 
-describe("/test endpoint", () => {
-    it("should return a response", async () => {
-        const response = await request.get("/test")
-        expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
-    })
-})
+async function test_app() {
+    console.log("test_app")
+}
+
+// describe("/test endpoint", () => {
+//     it("should return a response", async () => {
+//         const response = await request.get("/test")
+//         expect(response.status).toBe(200)
+//         expect(response.text).toBe("Hello world");
+//     })
+// })
