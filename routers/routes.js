d30386d015b9e7980b0caf4add0ac3ff55944c7f
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-05-03 15:33:18 +0300

    added test route

--- routers/routes.js
+++ routers/routes.js
@@ -21,4 +21,8 @@ router.post("/delete_journal_entries", (req, res) => {
   Journal_Entries.Delete(req, res);
 });
 
+app.get("/test", (_req, res) =>  {
+  res.status(200).send("Hello world")
+})
+
 module.exports = router;
